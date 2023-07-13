bb41c5277f3cebe797689c6a7edf986e2e154a10
Akshay4690 <ashaysa4690@gmail.com>
2023-07-14 03:20:41 +0530

    AddressBookUc3

--- AddressBook.java
+++ AddressBook.java
@@ -1,12 +1,16 @@
 package AssignAb3;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.Scanner;
 
 public class AddressBook {
 
 	ArrayList <Contacts> contact = new ArrayList<Contacts>();
+	
+	// Uc2 
 	
 	public static void addContact(List<Contacts> list)
 	{
@@ -44,5 +48,66 @@ public class AddressBook {
 		list.forEach(contacts -> System.out.println(contacts));
 		System.out.println();
 		
+	}
+	
+	// Uc3
+	
+	public static void editContact(List<Contacts> list)
+	{
+		boolean record = false;
+		Iterator<Contacts> iterator = list.iterator();
+		while (iterator.hasNext()) 
+		{
+			System.out.println(iterator.next());
+		}
+
+		System.out.print("Enter name to edit : ");
+		Scanner sc = new Scanner(System.in);
+		Scanner s1 = new Scanner(System.in);
+		String firstname = sc.next();
+
+		ListIterator<Contacts> iterator1 = list.listIterator();
+		
+		while (iterator1.hasNext())
+		{
+			Contacts c = iterator1.next();
+			if (c.getFirstname().equals(firstname)) 
+			{
+
+				System.out.print("Enter the last name : ");
+				c.setLastname(sc.next());
+		
+				System.out.print("Enter address name : ");
+				c.setAddress(sc.next());
+		
+				System.out.print("Enter the city name: ");
+				c.setCity(sc.next());
+		
+				System.out.print("Enter the state name : ");
+				c.setState(sc.next());
+		
+				System.out.print("Enter the zip number : ");
+				c.setZip(sc.nextInt());
+		
+				System.out.print("Enter the phone number : ");
+				c.setPhoneno(sc.nextLong());
+		
+				System.out.print("Enter the email : ");
+				c.setEmail(sc.next());
+		
+				record = true;
+			}
+		}
+		
+		if (record) 
+		{
+			System.out.println("Contact updated Successfully");
+			list.forEach(contacts -> System.out.println(contacts));
+		} 
+		else
+		{
+			System.out.println("Contact not found");
+			System.out.println();
+		}
 	}
 }
--- AddressBookMain.java
+++ AddressBookMain.java
@@ -2,16 +2,49 @@ package AssignAb3;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class AddressBookMain 
 {
 	public static void main(String[] args) 
 	{
+		System.out.println("Welcome to address book");
+		System.out.println();
+
 		List<Contacts> list = new ArrayList<>();
-		
-		AddressBook.addContact(list);
-		
-		System.out.println("WelCome to AddressBook Programm ");
+
+		boolean tree = true;
+
+		while (tree) {
+	
+			System.out.println("1. Add a new contact");
+			System.out.println("2. Edit an existing contact");
+			System.out.println("10. Exit \n");
+			System.out.print("Enter your choice : ");
+			Scanner sc = new Scanner(System.in);
+			int c = sc.nextInt();
+		System.out.println();
+
+		switch (c) {
+
+			case 1:
+				AddressBook.addContact(list);
+				break;
+			case 2:
+				AddressBook.editContact(list);
+				break;
+				
+			case 10:
+				tree = false;
+				System.out.println("exit successfull...");
+				break;
+
+			default:
+				System.out.println("Invalid choice please Enter the valid choice \n");
+
+		}
+
+		}
 	}
 
 }
